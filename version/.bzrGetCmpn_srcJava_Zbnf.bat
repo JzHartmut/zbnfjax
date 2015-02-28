aa839f8f4276c90c7c27238b2b265fd35eaf8ad0
Hartmut Schorrig <hartmut@vishia.org>
2015-03-01 00:00:00 +0100

    zbnf.jar: used for StateMGen, works   StateMGen improved, works   Now source version contained in subdir version

---
+++ version/.bzrGetCmpn_srcJava_Zbnf.bat
@@ -0,0 +1,2 @@
+echo checkouts bzr from any archive, bzrGetCmpn should be able to find as script in path, it should know where to find the archive.
+bzrGetCmpn "hartmut@labor-nb-20150301213648-8vbrut8oacrdzi9s" CHECKOUT
---
+++ version/readme.txt
@@ -0,0 +1,2 @@
+This directory contains information about the source version of zbnf.jar and Xslt.jar.
+The sources are contained in a bazaar archive which is able to download from www.vishia.org.
