4cad5b8f11ccfaf73d7d714b0771219ae1e449f2
Hartmut Schorrig <hartmut@vishia.org>
2015-11-07 00:00:00 +0100

    * States.genC1.jzgen

--- version/.bzrGetCmpn_srcJava_Zbnf.bat
+++ version/.bzrGetCmpn_srcJava_Zbnf.bat
@@ -1,2 +1,2 @@
 echo checkouts bzr from any archive, bzrGetCmpn should be able to find as script in path, it should know where to find the archive.
-bzrGetCmpn "hartmut@labor-nb-20150301213648-8vbrut8oacrdzi9s" CHECKOUT
+  bzrGetCmpn "hartmut@labor-nb-20150829224236-yaywx1j394cxhfjh" CHECKOUT
--- version/.bzrGetCmpn_srcJava_vishiaBase.bat
+++ version/.bzrGetCmpn_srcJava_vishiaBase.bat
@@ -1,2 +1,2 @@
 echo checkouts bzr from any archive, bzrGetCmpn should be able to find as script in path, it should know where to find the archive.
-bzrGetCmpn "hartmut@labor-nb-20150301213610-r8u4trlyw30j7xvy" CHECKOUT
+  bzrGetCmpn "hartmut@vishia.org-20150829224039-mco8nryqif9izmzo" CHECKOUT
